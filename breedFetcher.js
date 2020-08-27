6823dd44c43507dc4f3e985a8038bb04d326f99c
Andre Iskandar <andre.m.iskandar@gmail.com>
2020-08-27 13:56:13 -0700

    completed

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,5 +1,23 @@
 const request = require('request');
-const catURI = 'https://api.thecatapi.com/v1/images/search';
-const APIKEY = '67f60cab-61ca-403b-972a-cb3971f1475d';
+const { catURI } = require('./constants');
 
-const catApi = request();
+const breedInput = process.argv.slice(2)[0];
+const breedSearchURI = catURI + breedInput;
+
+// const getAPIbody = callback =>
+request(breedSearchURI, (error, response, body) => {
+	//what if breed is not found
+	if (error) {
+		return console.log(`${error}, Breed is not found`);
+		//what if typo domain
+	} else if (response.statusCode !== 200) {
+		return console.log(`HTTP code: ${response.statusCode}`);
+	}
+	const data = JSON.parse(body);
+	const breed = data[0];
+	if (!breed) {
+		return console.log('Breed not found');
+	} else {
+		return console.log(`${breed.description}`);
+	}
+});
