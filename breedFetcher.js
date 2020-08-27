e69c8e0826aaa270c78ab2eb071b1ae26d11ae22
Andre Iskandar <andre.m.iskandar@gmail.com>
2020-08-27 12:32:01 -0700

    starting point

---
+++ breedFetcher.js
@@ -0,0 +1,5 @@
+const request = require('request');
+const catURI = 'https://api.thecatapi.com/v1/images/search';
+const APIKEY = '67f60cab-61ca-403b-972a-cb3971f1475d';
+
+const catApi = request();
---
+++ constants.js
@@ -0,0 +1,7 @@
+const catURI = 'https://api.thecatapi.com/v1/breeds/search?q=';
+const APIKEY = '67f60cab-61ca-403b-972a-cb3971f1475d';
+// The best & most secure way to send it
+// Set your API Key as the x-api-key header on evey request.
+// e.g headers[“x-api-key”] = "ABC123"
+
+module.exports = { catURI, APIKEY };
